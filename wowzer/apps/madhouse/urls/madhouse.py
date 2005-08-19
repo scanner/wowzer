a697c815a23f4b42ad4b2f7b3289b34c45dd7362
Scanner <scanner@apricot.com>
2005-08-19 00:37:18 +0000

    oops. forgot a ','
    git-svn-id: file:///Volumes/extra1/backups/locker/repository/as-subversion/head@957 b85eb097-2c14-0410-a966-eec4f2146687

--- wowzer/apps/madhouse/urls/madhouse.py
+++ wowzer/apps/madhouse/urls/madhouse.py
@@ -6,8 +6,7 @@ urlpatterns = patterns(
     'wowzer.apps.madhouse.views',
     (r'^$', 'madhouse.index'),
     (r'^detail/(?P<auction_id>\d+)/$', 'auction.detail'),
-    (r'^history/(?P<item_id>\d+)/(?P<realm_id>\d+)/(?P<faction_id>\d+)$',
-     'history.for_realm_for_faction')
+    (r'^history/(?P<item_id>\d+)/(?P<realm_id>\d+)/(?P<faction_id>\d+)$', 'history.for_realm_for_faction'),
     (r'^submit/', 'submit.submit'),
     (r'^poke_ud_queue/', 'submit.poke_ud_queue'),
     # Example:

8a127da34606507ba481640c84db68a21cefd3b1
Scanner <scanner@apricot.com>
2005-08-19 00:21:24 +0000

    add hook for the items app in the main urls
    git-svn-id: file:///Volumes/extra1/backups/locker/repository/as-subversion/head@956 b85eb097-2c14-0410-a966-eec4f2146687

--- wowzer/settings/urls/main.py
+++ wowzer/settings/urls/main.py
@@ -1,9 +1,10 @@
+# File: $Id$
+#
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns(
     '',
     (r'^madhouse/', include('wowzer.apps.madhouse.urls.madhouse')),
     (r'^toons/', include('wowzer.apps.toons.urls.toons')),
-    # Example:
-    # (r'^wowzer/', include('wowzer.apps.foo.urls.foo')),
+    (r'^items/', include('wowzer.apps.items.urls.items')),
     )

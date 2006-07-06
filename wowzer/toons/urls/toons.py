019d75b97625cfe36c173e9b3b8b08d9d3da257a
Scanner <scanner@apricot.com>
2006-07-06 20:41:49 +0000

    remove toons urls directory
    git-svn-id: file:///Volumes/extra1/backups/locker/repository/as-subversion/wowzer/head@1060 b85eb097-2c14-0410-a966-eec4f2146687

--- wowzer/toons/urls/toons.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns(
-    'wowzer.apps.toons.views',
-    (r'^$', 'toons.index'),
-    (r'^detail/(?P<toon_id>\d+)/$', 'toons.detail'),
-    # Example:
-    # (r'^wowzer/', include('wowzer.apps.foo.urls.foo')),
-)

750437ff1628631f32901e0c3c58028a55180a0c
Scanner <scanner@apricot.com>
2007-03-01 09:21:59 +0000

    Fixing the top level forum url, more stuff for our script to test forums a bit.
    git-svn-id: file:///Volumes/extra1/backups/locker/repository/as-subversion/wowzer/head@1167 b85eb097-2c14-0410-a966-eec4f2146687

--- data/forum-init.py
+++ data/forum-init.py
@@ -2,20 +2,29 @@
 # Small script to initialize asforums with some data
 #
 from django.contrib.auth.models import User
-from wowzer.asforums.models import Forum
+from wowzer.asforums.models import *
 
-try:
-    u = User.objects.get(username='scanner')
-except User.DoesNotExist:
-    u = User(username="scanner", is_staff = True, is_active = True)
-    u.save()
+u, created = User.objects.get_or_create(username = 'scanner',
+                                        defaults = { 'is_staff' : True,
+                                                     'is_active' : True })
 
-for name in ('test1', 'test2', 'test3', 'test4'):
-    try:
-        f = Forum.objects.get(name=name)
-    except Forum.DoesNotExist:
-        f = Forum(name=name, slug = name, creator = u)
-        f.save()
+for name in ('Test','Test2'):
+    fc, ign = ForumCollection.objects.get_or_create(name = name, \
+                                     defaults = { 'slug' : name,
+                                                  'blurb' : 'Test collection',
+                                                  'creator' : u })
+    for name in ('test1', 'test2', 'test3', 'test4'):
+        f, ign = Forum.objects.get_or_create(name = name,
+                                        defaults = { 'slug' : name,
+                                                     'collection' : fc,
+                                                     'creator' : u })
         print "Created forum '%s'" % str(f)
-
-
+        for dname in ('disc1','disc2','disc3'):
+            d,created = Discussion.objects.get_or_create(name = name,
+                                                         forum = f,
+                                                 defaults = { 'slug' : name,
+                                                              'creator' : u})
+            if created:
+                for i in range(20):
+                    p = Post.objects.create(creator = u, discussion = d,
+                                            post = "Test test test test")

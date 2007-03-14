762d82c463833f8ab3fa9411d0777443c24f5be1
Scanner <scanner@apricot.com>
2007-03-14 00:57:46 +0000

    more moving around. Trying to actually make views now.
    git-svn-id: file:///Volumes/extra1/backups/locker/repository/as-subversion/wowzer/head@1172 b85eb097-2c14-0410-a966-eec4f2146687

--- wowzer/asforums/views.py
+++ wowzer/asforums/views.py
@@ -15,6 +15,9 @@ from django.core.paginator import ObjectPaginator, InvalidPage
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic.list_detail import object_list
 from django.views.generic.list_detail import object_detail
+from django.views.generic.create_update import create_object
+from django.views.generic.create_update import update_object
+from django.views.generic.create_update import delete_object
 from django.template.loader import get_template
 from django.template import RequestContext as Context
 from django.http import Http404
@@ -74,17 +77,19 @@ def fc_detail(request, fc_id):
 
 ############################################################################
 #
-def fc_update(request):
+@login_required
+def fc_update(request, fc_id):
     """A list of forum collections that the user can view.
     XXX For now until we get the form filled and our such we are going to
     XXX hand stuff off to the generic crud views. I am doing it this way
     XXX instead of entirely in the urls.py file because I am going to put
     XXX stuff here and I feel better with this stub defined.
     """
-    pass
+    return update_object(request, ForumCollection, fc_id)
 
 ############################################################################
 #
+@login_required
 def fc_delete(request):
     """A list of forum collections that the user can view.
     XXX For now until we get the form filled and our such we are going to
@@ -92,24 +97,33 @@ def fc_delete(request):
     XXX instead of entirely in the urls.py file because I am going to put
     XXX stuff here and I feel better with this stub defined.
     """
-    pass
+    return delete_object(request, ForumCollection,
+                         "/asforums/forum_collections/",
+                         object_id = fc_id)
 
 ############################################################################
 #
-def forum_list(request):
-    """A view that shows the forums that a person can see."""
+def obj_list_redir(request):
+    """Because of how objects in the asforum app are organized there is
+    no way to blindly list any one set of objects except the top level
+    hiearchy - the forum collection. So attempts to list any object
+    type except a forum collection results in a redirect to the
+    closest thing doing what the user may be after - the top level
+    index that lists collections, forums, etc."""
 
-    order_by = request.GET.get('order_by', 'created_at')
-    if order_by not in ('name', 'slug', 'creator', 'created_at',
-                        'last_post_at'):
-        order_by = 'created_at'
+    return HttpResponseRedirect("/asforums/")
 
-    query_set = Forum.objects.all().order_by(order_by)
-    ec = { 'order_by' : order_by }
-
-    return object_list(request, query_set,
-                       template_name = 'asforums/forum_list.html',
-                       extra_context = ec)
+############################################################################
+#
+@login_required
+def forum_create(request,fc_id):
+    """Creation of a new forum.
+    XXX For now until we get the form filled and our such we are going to
+    XXX hand stuff off to the generic crud views. I am doing it this way
+    XXX instead of entirely in the urls.py file because I am going to put
+    XXX stuff here and I feel better with this stub defined.
+    """
+    return create_object(request, Forum)
 
 ############################################################################
 #
@@ -122,21 +136,15 @@ def forum_detail(request, forum_id):
     except Forum.DoesNotExist:
         raise Http404
 
-    return object_detail(request, Forum.objects.all(), object_id = forum_id)
+    ec = { 'forum' : forum }
+
+    return object_list(request, Discussion.objects.filter(forum = forum)
+                       template_name = "asforums/forum_detail.html",
+                       extra_context = ec)
 
 ############################################################################
 #
-def forum_create(request):
-    """Creation of a new forum.
-    XXX For now until we get the form filled and our such we are going to
-    XXX hand stuff off to the generic crud views. I am doing it this way
-    XXX instead of entirely in the urls.py file because I am going to put
-    XXX stuff here and I feel better with this stub defined.
-    """
-    return create_object(request, Forum)
-
-############################################################################
-#
+@login_required
 def forum_update(request, forum_id):
     """Modify an existing forum object.
     XXX For now until we get the form filled and our such we are going to
@@ -144,10 +152,11 @@ def forum_update(request, forum_id):
     XXX instead of entirely in the urls.py file because I am going to put
     XXX stuff here and I feel better with this stub defined.
     """
-    return update_object(request, Forum, slug = forum_id, slug_field="slug")
+    return update_object(request, Forum, object_id = forum_id)
 
 ############################################################################
 #
+@login_required
 def forum_delete(request, forum_id):
     """Ask for confirmation if this is a GET, and if it is a POST we the
     proper bits set, delete the forum.
@@ -159,40 +168,156 @@ def forum_delete(request, forum_id):
     XXX instead of entirely in the urls.py file because I am going to put
     XXX stuff here and I feel better with this stub defined.
     """
-    forum = Forum.objects.get(slug = forum_id)
-
-    # After delete redirect to the forum collection this forum was in
-    #
-    return delete_object(request, Forum, post_delete_redirect = \
-                         forum.collection.get_absolute_url()
-                         slug = forum_id, slug_field="slug")
-
-############################################################################
-#
-def forum_disc_list(request, forum_id):
-    """A forum/discussion list. Basically lists some of the forum detail and
-    a list of discussions in that forum (filtered and sorted according to the
-    queryset.)
-    """
     try:
-        forum = Forum.objects.get(pk=forum_id)
+        forum = Forum.objects.get(pk = forum_id)
+        fc = forum.collection
     except Forum.DoesNotExist:
         raise Http404
 
-    # Orders the list of discussions in this forum.
+    # After delete redirect to the forum collection this forum was in
     #
-    order_by = request.GET.get('order_by', 'created_at')
-    if order_by not in ('name', 'slug', 'creator', 'created_at',
-                        'last_post_at'):
-        order_by = 'created_at'
+    return delete_object(request, Forum,
+                         post_delete_redirect = fc.get_absolute_url(),
+                         object_id = forum_id)
 
-    # Get the list of discussions in this forum that match our filter and
-    # ordering criteria.
-    #
-    query_set = Forum.objects.all().order_by(order_by)
-    ec = { 'order_by' : order_by, 'forum' : forum }
+############################################################################
+#
+@login_required
+def disc_create(request, forum_id):
+    """Creation of a new discussion.
+    XXX For now until we get the form filled and our such we are going to
+    XXX hand stuff off to the generic crud views. I am doing it this way
+    XXX instead of entirely in the urls.py file because I am going to put
+    XXX stuff here and I feel better with this stub defined.
+    """
+    return create_object(request, Discussion)
 
-    return object_list(request, query_set,
-                       template_name = 'asforums/forum_disc_list.html',
+############################################################################
+#
+def disc_detail(request, disc_id):
+    """A discussion detail shows the discussion details and a list of
+    all posts that match the filter/sort criteria.
+    """
+    try:
+        disc = Discussion.objects.get(pk=disc_id)
+    except Discussion.DoesNotExist:
+        raise Http404
+
+    ec = { 'discussion' : disc }
+
+    return object_list(request, Post.objects.filter(discussion = disc)
+                       template_name = "asforums/disc_detail.html",
                        extra_context = ec)
 
+############################################################################
+#
+@login_required
+def disc_update(request, disc_id):
+    """Modify an existing discussion object.
+    XXX For now until we get the form filled and our such we are going to
+    XXX hand stuff off to the generic crud views. I am doing it this way
+    XXX instead of entirely in the urls.py file because I am going to put
+    XXX stuff here and I feel better with this stub defined.
+    """
+    return update_object(request, Discussion, object_id = disc_id)
+
+############################################################################
+#
+@login_required
+def disc_delete(request, disc_id):
+    """Ask for confirmation if this is a GET, and if it is a POST we the
+    proper bits set, delete the discussion.
+
+    This will permanently delete all posts this discussion.
+
+    XXX Only a moderator and the super user can delete this.
+    XXX The owner can only 'shutdown' it, which is makes it unviewable
+    XXX and unpostable by everyone but moderators and the super user.
+    
+    XXX For now until we get the form filled and our such we are going to
+    XXX hand stuff off to the generic crud views. I am doing it this way
+    XXX instead of entirely in the urls.py file because I am going to put
+    XXX stuff here and I feel better with this stub defined.
+    """
+    try:
+        disc = Discussion.objects.get(pk = disc_id)
+        forum = disc.forum
+    except Forum.DoesNotExist:
+        raise Http404
+
+    # After delete redirect to the forum detail this discussion was in.
+    #
+    return delete_object(request, Discussion,
+                         post_delete_redirect = forum.get_absolute_url(),
+                         object_id = disc_id)
+
+
+############################################################################
+#
+@login_required
+def post_create(request, disc_id):
+
+    try:
+        disc = Discussion.objects.get(pk = disc_id)
+        forum = disc.forum
+        fc = forum.collection
+    except Discussion.DoesNotExist:
+        raise Http404
+
+    manipulator = Post.AddManipulator()
+    if request.POST:
+        # XXX Does user have 'post' permission in this discussion,
+        # XXX forum, forum collection (they need to explicitly have
+        # XXX the permission at some level.)
+        # if not request.user.has_perm():
+        #     raise HttpResponseForbidden("discussion")
+        #     raise HttpResponseForbidden("forum")
+        #     raise HttpResponseForbidden("forum collection")
+        new_data = request.POST.copy()
+        new_data['discussion'] = disc.id
+            
+        # Check for errors
+        errors = manipulator.get_validation_errors(new_data)
+        manipulator.do_html2python(new_data)
+
+        if not errors:
+            # No errors -- this means we can save the data!
+            new_object = manipulator.save(new_data)
+
+            if request.user.is_authenticated():
+                request.user.message_set.create(\
+                    message=gettext("The %(verbose_name)s was created "
+                                    "successfully.") % \
+                    {"verbose_name": model._meta.verbose_name})
+
+            return HttpResponseRedirect(new_object.get_absolute_url())
+    else:
+        # No POST, so we want a brand new form without any data or errors
+        errors = {}
+        new_data = manipulator.flatten_data()
+
+    # Create the FormWrapper, template, context, response
+    form = oldforms.FormWrapper(manipulator, new_data, errors)
+    t = get_template("asforums/post_form.html")
+    c = Context(request, {
+            'discussion' : disc,
+            'form'       : form,
+            })
+    return HttpResponse(t.render(c))
+
+############################################################################
+#
+def post_detail(request, post_id):
+    pass
+
+############################################################################
+#
+@login_required
+def post_update(request, post_id):
+    pass
+
+############################################################################
+#
+@login_required
+def post_delete(request, post_id):
+    pass

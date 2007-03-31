bd13885c1927fc5696524f187ccee9fd6173677f
Scanner <scanner@apricot.com>
2007-03-31 00:52:26 +0000

    We need to start writing very fancy tags of our own to do appropriate permission checking.
    git-svn-id: file:///Volumes/extra1/backups/locker/repository/as-subversion/wowzer/head@1199 b85eb097-2c14-0410-a966-eec4f2146687

--- wowzer/asforums/templatetags/asforums_tags.py
+++ wowzer/asforums/templatetags/asforums_tags.py
@@ -50,3 +50,112 @@ class ViewableByUserNode(template.Node):
             # call stack.
             #
             return ""
+
+##############################################################################
+#
+def has_perm_chain(parser, token):
+    """This custom tag deal with per object permissions and the permission chain
+    required for the asforum system. For things like 'post' and 'read'
+    permissions on a discussion, the user must have the permissions from all
+    containing objects as well. ie: in order to post to a discussion they must
+    have post permission on the discussion and post permission on the forum
+    and post permission on the forum collection.
+
+    This helper will use the permission inheritance map from the models file
+    to check to see if a user has the permissions necessary for this
+    object.
+
+    This will also support an 'or' syntax so you can do things like 'if user
+    has edit permission on this post or moderate permission on this forum they
+    can edit this post.
+
+    Checks permission on the given user. Checks row-level permissions if an
+    object is given.
+
+    Perm name should be in the format [app_label].[perm codename].
+    """
+    bits = token.contents.split()
+    del bits[0]
+    if not bits:
+        raise TemplateSyntaxError, "'if' statement requires at least one argument"
+    # bits now looks something like this: ['a', 'b', 'or', 'not', 'b', 'c',
+    # 'or', 'c.d', 'e.f']
+    #
+    # See if we have any 'and' or 'or' statements. THey can only use one or
+    # the other in a single tag.
+    #
+    bitstr = ' '.join(bits)
+    boolpairs = bitstr.split(' and ')
+    boolvars = []
+    if len(boolpairs) == 1:
+        link_type = HasPermChainNode.LinkTypes.or_
+        boolpairs = bitstr.split(' or ')
+    else:
+        link_type = HasPermChainNode.LinkTypes.and_
+        if ' or ' in bitstr:
+            raise TemplateSyntaxError, "'if' tags can't mix 'and' and 'or'"
+    
+    # Make sure each 'boolpairs' set is one two or three terms. If it is three
+    # terms the first must be 'not'. If it is two terms the first is a
+    # permission and the second is an object instance. If it is one term it is
+    # a permission (for all objects of that class)
+    #
+    for boolpair in boolpairs:
+        if ' ' in boolpair:
+            elts = boolpair.split()
+            if elts == 3:
+                if elts[0] != 'not':
+                    raise TemplateSyntaxError, "Expected 'not' in if statement"
+                else:
+                    pass
+                
+    
+    
+
+    
+class HasPermChainNode(Node):
+    def __init__(self, bool_exprs, nodelist_true, nodelist_false, link_type):
+        self.bool_exprs = bool_exprs
+        self.nodelist_true, self.nodelist_false = nodelist_true, nodelist_false
+        self.link_type = link_type
+
+    def __repr__(self):
+        return "<If node>"
+
+    def __iter__(self):
+        for node in self.nodelist_true:
+            yield node
+        for node in self.nodelist_false:
+            yield node
+
+    def get_nodes_by_type(self, nodetype):
+        nodes = []
+        if isinstance(self, nodetype):
+            nodes.append(self)
+        nodes.extend(self.nodelist_true.get_nodes_by_type(nodetype))
+        nodes.extend(self.nodelist_false.get_nodes_by_type(nodetype))
+        return nodes
+
+    def render(self, context):
+        if self.link_type == IfNode.LinkTypes.or_:
+            for ifnot, bool_expr in self.bool_exprs:
+                try:
+                    value = bool_expr.resolve(context, True)
+                except VariableDoesNotExist:
+                    value = None
+                if (value and not ifnot) or (ifnot and not value):
+                    return self.nodelist_true.render(context)
+            return self.nodelist_false.render(context)
+        else:
+            for ifnot, bool_expr in self.bool_exprs:
+                try:
+                    value = bool_expr.resolve(context, True)
+                except VariableDoesNotExist:
+                    value = None
+                if not ((value and not ifnot) or (ifnot and not value)):
+                    return self.nodelist_false.render(context)
+            return self.nodelist_true.render(context)
+
+    class LinkTypes:
+        and_ = 0,
+        or_ = 1

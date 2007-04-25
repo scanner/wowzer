4f1c562ede5551b8a3fa779677c330da55c0dc58
Scanner <scanner@apricot.com>
2007-04-25 07:51:48 +0000

    The beginnings of our generic permission submission form.
    git-svn-id: file:///Volumes/extra1/backups/locker/repository/as-subversion/wowzer/head@1236 b85eb097-2c14-0410-a966-eec4f2146687

--- wowzer/main/views.py
+++ wowzer/main/views.py
@@ -13,18 +13,25 @@ from datetime import datetime, timedelta
 
 # Django imports
 #
-from django import forms
+from django import newforms as forms
+from django.newforms import widgets
 from django.shortcuts import get_object_or_404
 from django.core.paginator import ObjectPaginator, InvalidPage
+from django.core.exceptions import ObjectDoesNotExist
 from django.template.loader import get_template
 from django.template import RequestContext as Context
-from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 from django.http import HttpResponse
 from django.http import HttpResponseServerError
 from django.http import HttpResponseRedirect
 
 from django.contrib.auth.decorators import login_required
+
+# Django provided contrib models.
+#
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.modles import RowLevelPermission, Permission
+from django.contrib.auth.modles import User, Group
 
 # The data models from our apps:
 #
@@ -40,9 +47,35 @@ def index(request):
 
 #############################################################################
 #
-def rlp_edit(request, obj_id, ct_id):
-    """This is a generic view that is given an object id and the content type
-    of that object.
+class PermissionForm(forms.Form):
+    permission = forms.CharField(max_length = 128,
+                                 widget = widgets.HiddenInput())
+    user_or_group = forms.ChoiceField(choices = ("user", "group"),
+                                      label = "User or group", help_text = \
+                                      "To set whether you are adding a "
+                                      "permission for a user or a group.")
+    name = forms.CharField(max_length = 30, initial = "Username to permit",
+                           help_text = "Enter the name of the user or "
+                           "group you wish to add a permission for.")
+    permission = forms.IntegerField(help_text = "Select the permission you "
+                                    "wish to add.")
+    current_perms = forms.MulitpleChoiceField(label = "Current Permissions",
+                                              help_text = "Select the permissi"
+                                              "ons you wish to remove from "
+                                              "this object")
+
+    # XXX Hm.. I think we need to write a custom constructor that is
+    # XXX like "form_for_instance" except it uses a permission and the
+    # XXX permission form above to construct the custom form we need for
+    # XXX each permission.
+
+#############################################################################
+#
+def rlp_edit(request, obj, ct):
+    """This is a generic view that is given an object and the content
+    type of that object. It is meant to be wrapped by an actual view
+    that looks up the object and its content type and validates that
+    the user has permission to change the permissions on the object,
 
     
     In the 'GET' form will look up the permissions that object can have, and
@@ -53,4 +86,15 @@ def rlp_edit(request, obj_id, ct_id):
     set of entities that have a specific permission or adding some entity to
     a permission.
     """
-    pass
+
+    obj_perms = Permission.objects.select_related().filter(content_type = ct)
+    
+    # If this is a post they are either removing a permission, or
+    # adding a permission. We determine which by the permission field
+    # in the form submitted.
+    #
+    if request.method == "POST":
+        for perm in obj_perms:
+
+            if request.POST["permission"] == perm.codename:
+                form = PermissionForm

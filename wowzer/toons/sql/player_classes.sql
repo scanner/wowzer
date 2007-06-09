3aee4c77c562d0fcf7574153fe5b12b303a701e8
Scanner <scanner@apricot.com>
2007-06-09 08:43:28 +0000

    Add initial data for some of the toon models
    git-svn-id: file:///Volumes/extra1/backups/locker/repository/as-subversion/wowzer/head@1308 b85eb097-2c14-0410-a966-eec4f2146687

---
+++ wowzer/toons/sql/player_classes.sql
@@ -0,0 +1,9 @@
+INSERT INTO toons_playerclass (name, description) VALUES ('Mage', 'User of magic');
+INSERT INTO toons_playerclass (name, description) VALUES ('Priest', '');
+INSERT INTO toons_playerclass (name, description) VALUES ('Rogue', '');
+INSERT INTO toons_playerclass (name, description) VALUES ('Shaman', '');
+INSERT INTO toons_playerclass (name, description) VALUES ('Warlock', '');
+INSERT INTO toons_playerclass (name, description) VALUES ('Warrior', '');
+INSERT INTO toons_playerclass (name, description) VALUES ('Druid', '');
+INSERT INTO toons_playerclass (name, description) VALUES ('Hunter', '');
+INSERT INTO toons_playerclass (name, description) VALUES ('Paladin', '');
---
+++ wowzer/toons/sql/realms.sql
@@ -0,0 +1,1 @@
+INSERT INTO toons_realm (name,timezone) VALUES ("Argent Dawn", "PDT");
